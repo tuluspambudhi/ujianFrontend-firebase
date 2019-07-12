eb63f38876123b29d4b2f73a849a0c82223e2ec4
tuluspambudhi <jatipambudhi@gmail.com>
2019-07-12 17:55:28 +0700

    versi firebase

---
+++ deploy-ujian/public/precache-manifest.cfbc51d83b9d74bce161fcd7383fe2e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26163c08fd566e0b29ca2abebef632f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d2e23b5896fc37a3054",
+    "url": "/static/css/2.45c5f64b.chunk.css"
+  },
+  {
+    "revision": "ffa6e9da358b1f66961b",
+    "url": "/static/css/main.b02c7ca8.chunk.css"
+  },
+  {
+    "revision": "5d2e23b5896fc37a3054",
+    "url": "/static/js/2.dec13341.chunk.js"
+  },
+  {
+    "revision": "ffa6e9da358b1f66961b",
+    "url": "/static/js/main.c292bf3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
